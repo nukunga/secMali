4e4c1729e9310832db61fe608854946b3e23a7c6
nukunga <145279280+nukunga@users.noreply.github.com>
2023-10-12 11:36:30 +0900

    jsh

--- capstone_back/myapi/main.py
+++ capstone_back/myapi/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from typing import List
 
-app = FastAPI() # 인스턴스 생성
+app = FastAPI()
 
-@app.get("/") # get method로 '/'에 해당하는  생성
-def root():
-    return {'Hello':'World!'} 
+@app.post("/analyze/")
+async def analyze_files(files: List[UploadFile]):
+    analysis_results = []
+
+    for file in files:
+        # 파일을 어딘가에 저장하고 분석에 사용할 준비를 합니다.
+        with open(file.filename, "wb") as f:
+            f.write(file.file.read())
+
+        # YARA 및 olevba를 사용하여 파일을 분석하고 결과를 얻습니다.
+        # 이 부분은 YARA 및 olevba를 사용하여 분석하는 코드로 대체해야 합니다.
+
+        result = {"filename": file.filename, "analysis_results": analysis_results}
+        analysis_results.append(result)
+
+    return {"results": analysis_results}
