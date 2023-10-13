47b83a2dd871e2cdcb90847284f96b2b93035a3c
nukunga <145279280+nukunga@users.noreply.github.com>
2023-10-14 02:15:38 +0900

    # by jsh

--- capstone_back/myapi/main.py
+++ capstone_back/myapi/main.py
@@ -1,46 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 import yara
-from oletools.olevba import VBA_Parser
+import oletools.olevba
 
 app = FastAPI()
 
-# YARA 규칙 파일을 로드
-rules = yara.compile('C:\\Users\\User\\Documents\\GitHub\\secMali\\capstone_back\\myapi\\rules\\maldocs_index.yar') 
-
-@app.post("/scan_document/")
-async def scan_document(file: UploadFile): # DB서버 연동하여 파일 가져오기
-    content = await file.read()
-
-    # 파일을 YARA로 스캔
-    yara_matches = rules.match(data=content)
-
-    yara_matched_strings = []
-    for match in yara_matches:
-        yara_matched_strings.append({
-            "rule_name": match.rule,
-            "offset": match.strings[0][0],
-            "matched_content": match.strings[0][2],
-        })
-
-    if yara_matches:
-        return {"message": "Malicious document detected by YARA", "yara_matches": yara_matched_strings}
-
-    # 파일을 olevba로 스캔하여 매크로 추출
-    vba_parser = VBA_Parser(content)
-    macro_code = []
-    for (subfilename, stream_path, vba_filename, vba_code) in vba_parser.extract_macros():
-        macro_code.append({
-            "subfilename": subfilename,
-            "stream_path": stream_path,
-            "vba_filename": vba_filename,
-            "vba_code": vba_code
-        })
-
-    if macro_code:
-        return {"message": "Macros found in the document", "macros": macro_code}
-    else:
-        return {"message": "No macros found in the document"}
-
-#if __name__ == "__main__":
-#    import uvicorn
-#    uvicorn.run(app, host="127.0.0.1", port=8000)
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    
+    # Yara를 사용하여 악성 문서 판별
+    rules = yara.compile(filepath='rules/maldocs_index.yar')
+    matches = rules.match(data=contents)
+    
+    # 매칭된 룰의 이름 추출
+    matched_rules = [match.rule for match in matches]
+    
+    # Olevba를 사용하여 매크로 추출
+    vba = oletools.olevba.VBA_Parser(file.file, data=contents)
+    macros = []
+    if vba.detect_vba_macros():
+        for (filename, stream_path, vba_filename, vba_code) in vba.extract_macros():
+            macros.append({
+                "filename": filename,
+                "stream_path": stream_path,
+                "vba_filename": vba_filename,
+                "vba_code": vba_code
+            })
+    
+    return {"filename": file.filename, "matched_rules": matched_rules, "macros": macros}
